6e909469d094ea1533f0f7198144a15e1fd215de
jopoepl <joel_joseph93@outlook.com>
2022-05-12 13:28:42 +0530

    Assn: Functions

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -2,6 +2,10 @@
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
 
+function sayHello() {
+  alert(`Hello World`);
+}
+
 /*
 2. Create a function named `fullName` with the following steps:
 
@@ -10,6 +14,16 @@
   - Concat the value of fistName and lastName and store it in another variable named `fullName`.
   - Alert `fullName`
 */
+function fullName() {
+  let firstName = prompt(`Enter your first name`);
+  let lastName = prompt(`Enter your last name`);
+  let fullName = firstName + " " + lastName;
+  alert(fullName);
+}
+
+
+
+
 
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
@@ -19,6 +33,15 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
+
+function addTwoNumbers() {
+  let firstNum = +prompt(`Enter the first Number`);
+  let secondNum = +prompt(`Enter the second Number`);
+  let sum = firstNum + secondNum;
+  alert(sum);
+}
+
+
 
 /*
 4. Create a function named `getTable` with the following steps:
@@ -39,12 +62,30 @@
   9 * 10 = 90
 */
 
+function multiplicationTable() {
+  let num = +prompt(`Enter a Number`);
+  for (let i = 1; i <=10; i++) {
+    let mul = num * i;
+    console.log(mul);
+  }
+}
+
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
+
+function isLeapYear() {
+  let year = +prompt(`Enter a year`);
+  if (year % 4 === 0) {
+    alert(year + " " + `is a Leap Year`)
+  } else {
+    alert(year + " " + `is not a Leap Year`)
+  }
+}
+
 
 /*
 6. Create a function named `getFactorial` with the following steps:
@@ -53,3 +94,20 @@
   - Alert `The factorial of [number] is [factorial]`
 
 */
+
+function factorial(){
+let number = +prompt(`Enter a Number`);
+let i = number;
+let factorial = number;
+ while(i >= 2) {
+   i--;
+   factorial *= i;
+ }
+ alert(factorial);
+}
+
+
+
+
+
+
